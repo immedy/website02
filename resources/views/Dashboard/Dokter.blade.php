db324fda3b6d0ce3ef2edd22dc15761afc00e44b
dvg32 <devangogh32@gmail.com>
2022-11-28 10:16:01 +0800

    Update Dokter.blade.php

--- resources/views/Dashboard/Dokter.blade.php
+++ resources/views/Dashboard/Dokter.blade.php
@@ -36,10 +36,10 @@
                                     <p class=" mb-0">{{ $p->nama }}</p>
                                 </td>
                                 <td class="col-auto">
-                                    <p class=" mb-0">{{ $p->smf }}</p>
+                                    <p class=" mb-0">{{ $p->reff_smf_details->deskripsi }}</p>
                                 </td>
                                 <td class="col-auto">
-                                    <p class=" mb-0">{{ $p->poliklinik }}</p>
+                                    <p class=" mb-0">{{ $p->reff_poli_details->deskripsi }}</p>
                                 </td>
                                 <td class="col-auto">
                                     @if ($p->status = 1)
